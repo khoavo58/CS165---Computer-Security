48769ca18389c9afee2a9188e064d33dccf5f7aa
jt7898 <32527413+jt7898@users.noreply.github.com>
2020-01-17 11:08:12 -0800

    Update password_md5.cpp

--- password_md5.cpp
+++ password_md5.cpp
@@ -1,8 +1,45 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
 
-  return 0;
+	string HASH = "WT9A81ixE0ygG7v8y7ijG/"
+	string password = "";
+	string salt = "4fTghp6q";
+	string magic = "$1$";
+	
+	string alt_sum = md5(password + salt + password);
+	string int_sum = md5(password + magic + salt + alt_sum.substr(0, 6) + password[0] + \0 + \0);
+	
+	for(unsigned int i = 0; i < 1000; i++) {
+		
+		string temp = "";
+		
+		if(i % 2 == 0) {
+			temp += int_sum;
+		}
+		if(i % 2 == 1) {
+			temp += password;
+		}
+		if(!(i % 3 == 0)) {
+			temp += salt;
+		}
+		if(!(i % 7 == 0)) {
+			temp += password;
+		}
+		if(i % 2 == 0) {
+			temp += password;
+		}
+		if(i % 2 == 1) {
+			temp += int_sum;
+		}
+		
+		int_sum = md5(temp);
+	}
+	
+	cout << magic << salt << "$";
+	
+	return 0;
 }
